118dcdfca9357d818cfcd08768fb12f6e51423d8
Anderson Chu <chuducan.dev@gmail.com>
2025-02-08 00:17:16 +0700

    chore: update webpack config and add CSS/copy webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+import path from 'path';
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
 
-module.exports = {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
   entry: {
     popup: './src/popup/index.tsx',
     content: './src/content/index.tsx',
@@ -32,6 +37,11 @@ module.exports = {
       template: './src/popup/index.html',
       filename: 'popup.html',
       chunks: ['popup'],
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/manifest.json', to: 'manifest.json' },
+      ],
     }),
   ],
 }; 
